7cae05c357d9fa48b5b0d4b8ee1de095ef9e9e54
nigeymc <mckenna.niall@gmail.com>
2022-05-17 23:06:29 +0100

    sitemap added and google analytics plugin

--- frontity.settings.js
+++ frontity.settings.js
@@ -31,6 +31,14 @@ const settings = {
         }
       }
     },
+    {
+      name: "@frontity/google-analytics",
+      state: {
+        googleAnalytics: {
+          trackingId: "315556641",
+        },
+      },
+    },
     "@frontity/tiny-router",
     "@frontity/html2react",
     "@aamodtgroup/frontity-contact-form-7"
--- packages/jmk-theme/src/components/index.js
+++ packages/jmk-theme/src/components/index.js
@@ -65,7 +65,9 @@ const Theme = ({ state }) => {
         <meta property="og:type" content="website" />
         <meta property={state.frontity.description} />
         <meta property="og:url" content={`https://jmksecurity.net${data.route}`} />
-        <meta property="og:image" content="https://wp.jmksecurity.net/wp-content/uploads/2022/05/Screenshot-2022-05-10-at-5.44.28-pm.png" />
+        {state.theme.featured.showOnPost ? (
+          <meta property="og:image" content={post.featured_media} />
+        ) : (<meta property="og:image" content="https://wp.jmksecurity.net/wp-content/uploads/2022/05/Screenshot-2022-05-10-at-5.44.28-pm.png" />)}
         <html lang="en" />
         <meta name="robots" />
         <meta name="googlebot" />
