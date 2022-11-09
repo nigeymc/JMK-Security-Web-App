78e143235c9855ddb1634f0158e63224e023e2da
nigeymc <mckenna.niall@gmail.com>
2022-11-09 21:35:51 +0000

    description updated

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,35 +1,36 @@
 const settings = {
-  "name": "jmksecurity-app",
-  "state": {
-    "frontity": {
-      "url": "https://jmksecurity.net/",
-      "title": "JMK Security",
-      "description": "JMK SECURITY ARE TYRONE'S LEADING SECURITY & CCTV SYSTEMS SPECIALISTS IN DESIGN & INSTALLATION OF BESPOKE SECURITY & CCTV SYSTEMS THAT PROTECT HOMES, BUSINESSES & ASSETS ALL OVER IRELAND, NORTH AND SOUTH, COUNTRYWIDE. WE SPECIALISE IN CCTV TOWER HIRE AND WE ARE EXPERTS IN DOMESTIC AND COMMERCIAL CCTV SECURITY SYSTEMS INSTALLATION THROUGHOUT IRELAND AND THE UK. WE ALSO COMPLETE A WIDE VARIETY OF ELECTRICAL INSTALLATIONS AS A NICEIC APPROVED ELECTRICAL CONTRACTOR."
-    }
+  name: "jmksecurity-app",
+  state: {
+    frontity: {
+      url: "https://jmksecurity.net/",
+      title: "JMK Security",
+      description:
+        "Tyrone based JMK Security are an Irish CCTV and security systems installation business. We provide specialist security solutions including CCTV, access control, ANPR and intruder alarm systems for domestic and commercial customers throughout Northern Ireland, Ireland and the UK. We can design and install bespoke security and CCTV systems that protect homes, businesses and assets. We also offer CCTV mobile eco tower hire countrywide. As a NICEIC Approved Electrical Contractor we also complete a wide variety of electrical installations. We offer security solutions for homes, office, shop, factory and warehouse.",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [],
-          "featured": {
-            "showOnList": true,
-            "showOnPost": true
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [],
+          featured: {
+            showOnList: true,
+            showOnPost: true,
           },
-          "autoPrefetch": "in-view",
-        }
-      }
+          autoPrefetch: "in-view",
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://wp.jmksecurity.net/",
-          "homepage": "/homepage",
-          "postsPage": "/news/"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://wp.jmksecurity.net/",
+          homepage: "/homepage",
+          postsPage: "/news/",
+        },
+      },
     },
     {
       name: "@frontity/google-analytics",
@@ -41,8 +42,8 @@ const settings = {
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
-    "@aamodtgroup/frontity-contact-form-7"
-  ]
+    "@aamodtgroup/frontity-contact-form-7",
+  ],
 };
 
 export default settings;
